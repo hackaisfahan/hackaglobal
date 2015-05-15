26055a112c8a7d43ea29d01d5912d9c7bf5c1926
Milad Nekofar <milad@nekofar.com>
2015-05-15 10:37:03 +0430

    added redbean php as orm using composer

---
+++ app/configs/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Set default time zone
+date_default_timezone_set('Asia/Tehran');
+
+// Set display error settings
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+ini_set('short_open_tag', TRUE);
+
+// Set database config constants
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'hackaglobal');
